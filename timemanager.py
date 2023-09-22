58113cab475be66df769faf63ed6b23b7aac546d
XavierSAE <143823852+nnlclover@users.noreply.github.com>
2023-09-22 14:00:48 +0300

    Update timemanager.py

--- timemanager.py
+++ timemanager.py
@@ -19,7 +19,7 @@ def make_malling(message) -> None:
         if user["state"] == 200 or user["state"] == 400: 
             try:
                 print(user["second_name"])
-                text = f"Уважаемый {user['second_name']}! Через 5 минут у вас {message}."
+                text = f"Dear {user['second_name']}! {message}."
                 bot.sendSimpleMessage(user["chat_id"], message)
             except:
                 print("error send")
